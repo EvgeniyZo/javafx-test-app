8484e48e6b7a53cdd810c4f6559e225fe7a2d78c
teacher <teacher@nntc.nnov.ru>
2024-10-28 09:50:49 +0300

    javafx project template

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.javafxtestapp {
+    //requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.javafxtestapp to javafx.fxml;
+    exports com.example.javafxtestapp;
+}
